f432a4402085c9d03ff8b64aa63e05873641e964
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2020-06-05 10:41:38 -0500

    Updates

---
+++ precache-manifest.25835342c740ddf2dec30534eea8f2fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa396185ee501b319a98a62283319a9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "574b9be1a1111d59cb46",
+    "url": "/my-app/static/js/2.af7a8be1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.af7a8be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7883fb766d204692d720",
+    "url": "/my-app/static/js/main.9fdb71f5.chunk.js"
+  },
+  {
+    "revision": "5e5e7d0f00e95849687b",
+    "url": "/my-app/static/js/runtime-main.27caa03e.js"
+  }
+]);
